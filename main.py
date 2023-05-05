dde712275d4425debfb345afa2f68491cf3d4bed
moosavi <moosavi238@gmail.com>
2023-05-05 08:35:18 +0330

    add forward_difference

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from forward_difference import ForwardDifference
 from lagrange import Lagrange
 from neville import Neville
 from newton import Newton
 
 points = [(1, 5), (2, 0), (3, 5)]
+
+forward_difference = ForwardDifference(points)
+forward_difference.get_p().print()
+print()
+print(forward_difference.P(1.5))
 
 neville = Neville(points)
 neville.get_p().print()
