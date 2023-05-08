65cd6fb3f15a12eebd2d154fa5e478bc9040fa4f
moosavi <moosavi238@gmail.com>
2023-05-08 04:12:54 +0330

    add print

--- interpolation.py
+++ interpolation.py
@@ -5,9 +5,22 @@ class Interpolation:
     def __init__(self, points: list[(float, float)]):
         self._points = points.copy()
         self._p: Polynomial = Polynomial([])
+        self._n = len(points)
+        self._s: list = []
 
     def get_p(self) -> Polynomial:
         return self._p
 
     def P(self, x: float) -> float:
         return self._p.P(x)
+
+    def _print(self):
+        a = ""
+        for i in self._s:
+            if type(i) == Polynomial:
+                a += i.get_str() + ") +"
+            else:
+                if float(i) < 0:
+                    a = a[:-1]
+                a += str(i) + "("
+        print(f"P = {a[:-1]}= {self._p.get_str()}")
