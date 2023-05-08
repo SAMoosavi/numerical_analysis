61f72b643418e302853df05739b80286bd91126b
moosavi <moosavi238@gmail.com>
2023-05-08 04:15:32 +0330

    edit print

--- backward_difference.py
+++ backward_difference.py
@@ -1,39 +1,62 @@
 from interpolation import Interpolation
 from polynomial import Polynomial
+from prettytable import PrettyTable
 
 
 class BackwardDifference(Interpolation):
     def __init__(self, points: list[(float, float)]):
         super().__init__(points)
-        self.__ni: list[Polynomial] = [Polynomial([1])] * (len(self._points))
+        self.__ni: list[Polynomial] = [Polynomial([1])] * self._n
         self.__calculate_ni()
         self._p = Polynomial([0])
         self.__calculate_p()
+        self.__print()
 
     def __calculate_nfi(self, n: int, i: int):
-        a = 0
         if n == 0:
-            a = self._points[i][1]
+            return self._points[i][1]
         else:
-            a = self.__calculate_nfi(n - 1, i) - self.__calculate_nfi(n - 1, i - 1)
-        print(f"{n}f{i} = {a}")
-        return a
+            return self.__calculate_nfi(n - 1, i) - self.__calculate_nfi(n - 1, i - 1)
 
     def __calculate_ni(self):
         t, _ = Polynomial([1, -self._points[-1][0]]).quotient(Polynomial(
             [self._points[1][0] - self._points[0][0]]))
-        for i in range(1, len(self.__ni)):
+        for i in range(1, self._n):
             self.__ni[i], _ = self.__ni[i - 1].product(t.sum(Polynomial([i - 1]))).quotient(Polynomial([i]))
 
     def __calculate_p(self):
-        ai: list[float] = [0] * len(self.__ni)
-        for i in range(len(self.__ni)):
-            ai[i] = self.__calculate_nfi(i, len(self.__ni) - 1)
-        print("P = ", end='')
+        ai: list[float] = [0] * self._n
+        for i in range(self._n):
+            ai[i] = self.__calculate_nfi(i, self._n - 1)
         for i, val in enumerate(self.__ni):
-            print(ai[i], end='')
-            print(" ( ", end='')
-            val.print()
-            print(" ) + ", end='')
+            self._s.append(ai[i])
+            self._s.append(val)
             self._p = self._p.sum(val.product(Polynomial([ai[i]])))
-        print()
+
+    def __print(self):
+        print("================================")
+        print("Backward Difference")
+        print("d^nfi = d^(n - 1)fi - d^(n - 1)f(i - 1)")
+        table = PrettyTable()
+        for n in range(self._n):
+            col = []
+            for i in range(n):
+                col.append("")
+            for i in range(self._n - n, 0, -1):
+                col.append(self.__calculate_nfi(n, self._n - i))
+                col.append("")
+            for i in range(n):
+                col.append("")
+            col.pop()
+            header = ""
+            if n == 0:
+                header = ""
+            elif n == 1:
+                header = "d"
+            else:
+                header = f"d^{n}"
+            header += "fi"
+            table.add_column(header, col, align='c')
+        print(table)
+        self._print()
+        print("================================")
