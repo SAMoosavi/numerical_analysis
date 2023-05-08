2b16c485d660a84f5a9df0bc4f86616e90b90fa5
moosavi <moosavi238@gmail.com>
2023-05-08 04:56:55 +0330

    edit print

--- lagrange.py
+++ lagrange.py
@@ -11,6 +11,7 @@ class Lagrange(Interpolation):
         self.__calculate_li()
         self._p = Polynomial([0])
         self.__calculate_p()
+        self.__print()
 
     def __calculate_l(self) -> None:
         for i in self._points:
@@ -19,22 +20,19 @@ class Lagrange(Interpolation):
     def __calculate_li(self) -> None:
         for i, val in enumerate(self._points):
             self.__li[i], _ = self.__l.quotient(Polynomial([1, -val[0]]))
-            print(f"{val} = ", end="")
-            self.__li[i].print()
-            print("/", end='')
-            Polynomial([self.__li[i].P(val[0])]).print()
             self.__li[i], _ = self.__li[i].quotient(Polynomial([self.__li[i].P(val[0])]))
-            print(" = ", end='')
-            self.__li[i].print()
-            print()
 
     def __calculate_p(self) -> None:
-        print("P = ", end='')
         for i, val in enumerate(self._points):
-            Polynomial([val[1]]).print()
-            print(" ( ", end='')
-            self.__li[i].print()
-            print(" ) + ", end='')
+            self._s.append(val[1])
+            self._s.append(self.__li[i])
             self.__li[i].product(Polynomial([val[1]]))
             self._p = self._p.sum(self.__li[i].product(Polynomial([val[1]])))
-        print()
+
+    def __print(self):
+        print("================================")
+        print("Lagrange")
+        for i, val in enumerate(self._points):
+            print(f"{val} =({self.__li[i].get_str()}) / {self.__li[i].P(val[0])} = {self.__li[i].get_str()}")
+        self._print()
+        print("================================")
