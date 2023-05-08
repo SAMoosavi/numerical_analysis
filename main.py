04bb24c28270649b7f31ca03f1374a2d89d653f7
moosavi <moosavi238@gmail.com>
2023-05-08 05:16:34 +0330

    use new ui

--- main.py
+++ main.py
@@ -4,26 +4,21 @@ from lagrange import Lagrange
 from neville import Neville
 from newton import Newton
 
-points = [(1, 5), (2, 0), (3, 5)]
+points = [(-1, 0), (0, -1), (1, 0)]
 
 backward_difference = BackwardDifference(points)
-backward_difference.get_p().print()
-print()
 print(backward_difference.P(1.5))
 
 forward_difference = ForwardDifference(points)
-forward_difference.get_p().print()
-print()
 print(forward_difference.P(1.5))
 
 neville = Neville(points)
 neville.get_p().print()
+print()
 print(neville.P(1.5))
 
 newton = Newton(points)
-newton.get_p().print()
 print(newton.P(1.5))
 
 lagrange = Lagrange(points)
-lagrange.get_p().print()
 print(lagrange.P(1.5))
